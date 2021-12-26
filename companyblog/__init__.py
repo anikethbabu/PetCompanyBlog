0d4840efd7f49fc85c8192173aa3240f93173557
anikethbabu <anikethbabu@gmail.com>
2021-12-26 18:37:44 -0500

    same as previous commit

--- companyblog/__init__.py
+++ companyblog/__init__.py
@@ -7,6 +7,7 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = 'mysecret'
 # DataBase Setup
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
@@ -17,11 +18,15 @@ Migrate(app,db)
 
 # Login Configurations
 login_manager = LoginManager()
+
 login_manager.init_app(app)
 login_manager.login_view = 'users.login'
 
 # Regsiter views in __init__.py
 from companyblog.core.views import core
+from companyblog.users.views import users
 from companyblog.error_pages.handlers import error_pages
+
 app.register_blueprint(core)
+app.register_blueprint(users)
 app.register_blueprint(error_pages)
